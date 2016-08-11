021d1cedf23a98b3896b39c3e2038fa32ff63d7a
Pere Viader <Pere Viader>
2016-08-11 22:15:56 +0200

    Better camera now offers both linear and circle movement
    Signed-off-by: Pere Viader <Pere Viader>

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -36,6 +36,9 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]
     Vector3 lookAtOffset;
 
+    [SerializeField]
+    bool useLinearCamera;
+
     float currentHorizontalRotation;
     float currentVerticalRotation;
 
@@ -63,7 +66,6 @@ public class CameraFollow : MonoBehaviour
                 horizontal = horizontalSensitivity * Input.GetAxis("CameraHorizontal");
                 vertical = verticalSensitivity * Input.GetAxis("CameraVertical");
             }
-                
 
             currentVerticalRotation += (invertVertical * vertical) % 360.0f;
             currentVerticalRotation = Mathf.Clamp(currentVerticalRotation, minimumVerticalAngle, maximumVerticalAngle);
@@ -71,8 +73,14 @@ public class CameraFollow : MonoBehaviour
             currentHorizontalRotation += (invertHorizontal * horizontal) % 360.0f;
 
             Vector3 desiredPositionFromObject = Quaternion.Euler(currentVerticalRotation, currentHorizontalRotation, 0f) * (Vector3.back * distanceFromObject);
-            Vector3 currentPositionFromObject = transform.position - position;
-            transform.position = Vector3.Slerp(currentPositionFromObject, desiredPositionFromObject, lerpFactor * Time.fixedDeltaTime) + position;
+            if ( useLinearCamera )
+            {
+                transform.position = Vector3.Lerp(transform.position, position + desiredPositionFromObject, lerpFactor * Time.fixedDeltaTime);
+            } else
+            {
+                Vector3 currentPositionFromObject = transform.position - position;
+                transform.position = Vector3.Slerp(currentPositionFromObject, desiredPositionFromObject, lerpFactor * Time.fixedDeltaTime) + position;
+            }
             lastPosition = position;
         }
     }
