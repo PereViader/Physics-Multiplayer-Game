5fe44737c2e00015061b881636cf6c1728394606
Pere Viader <Pere Viader>
2016-08-16 18:14:20 +0200

    UI scaling should now work
    playerfollower teleports if distance is great fet per els tubs teleportadors

--- Assets/Scripts/PlayerFollower.cs
+++ Assets/Scripts/PlayerFollower.cs
@@ -9,10 +9,21 @@ public class PlayerFollower : MonoBehaviour {
     [SerializeField]
     private float lerpFactor = 0.1f;
 
+    [SerializeField]
+    float teleportDistance;
+
+    [SerializeField]
+    bool canTeleport;
+
     void FixedUpdate()
     {
         if (player)
-            transform.position = Vector3.Lerp(transform.position, player.position, lerpFactor * Time.fixedDeltaTime);
+        {
+            if (canTeleport && Vector3.Distance(transform.position, player.position) > teleportDistance)
+                transform.position = player.position;
+            else
+                transform.position = Vector3.Lerp(transform.position, player.position, lerpFactor * Time.fixedDeltaTime);
+        }
         else
             Destroy(gameObject);
     }
